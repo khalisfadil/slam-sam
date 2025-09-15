c1f7faa36db59c59e36eb952d72653245ed11be1
khalisfadil <muhammad.khalis.fadil@gmail.com>
2025-09-15 14:10:17 +0200

    update

--- run/viz_lidar_udp.cpp
+++ run/viz_lidar_udp.cpp
@@ -134,8 +134,8 @@ int main() {
             // Apply the (x, y, z) -> (y, x, -z) transform to match the PCL viewer's frame.
             for (auto& point : cloud.points) {
                 float original_x = point.x;
-                point.x = point.y;    // New X is Old Y (East -> PCL Right)
-                point.y = original_x; // New Y is Old X (North -> PCL Up)
+                point.x = -point.y;    // New X is Old Y (East -> PCL Right)
+                point.y = -original_x; // New Y is Old X (North -> PCL Up)
                 point.z = point.z;   // New Z is -Old Z (Down -> PCL Out of screen)
             }
             // --- End of new code ---
