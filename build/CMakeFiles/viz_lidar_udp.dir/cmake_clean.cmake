72badd2506eee301a4240e13c8c2b2439d97eebc
khalisfadil <muhammad.khalis.fadil@gmail.com>
2025-09-15 11:32:15 +0200

    first commit

---
+++ build/CMakeFiles/viz_lidar_udp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/viz_lidar_udp.dir/run/viz_lidar_udp.cpp.o"
+  "CMakeFiles/viz_lidar_udp.dir/run/viz_lidar_udp.cpp.o.d"
+  "CMakeFiles/viz_lidar_udp.dir/src/lidarcallback.cpp.o"
+  "CMakeFiles/viz_lidar_udp.dir/src/lidarcallback.cpp.o.d"
+  "viz_lidar_udp"
+  "viz_lidar_udp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/viz_lidar_udp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/viz_lidar_udp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for viz_lidar_udp.
+# This may be replaced when dependencies are built.
