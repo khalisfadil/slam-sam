73f7db6c33a3c89c28f5fed606023524b86a62b2
khalisfadil <muhammad.khalis.fadil@gmail.com>
2025-09-23 21:02:31 +0200

    update

--- run/pipeline.cpp
+++ run/pipeline.cpp
@@ -314,8 +314,8 @@ int main() {
                 }
 
                 Eigen::Matrix4d Tb2m = Eigen::Matrix4d::Identity();
-                Tb2m.block<3,3>(0,0) = Cb2m.transpose();
-                Tb2m.block<3,1>(0,3) = tm2b;
+                Tb2m.block<3,3>(0,0) = Cb2m;
+                Tb2m.block<3,1>(0,3) = -tm2b;
 
                 pcl::PointCloud<pcl::PointXYZI>::Ptr pointsMap(new pcl::PointCloud<pcl::PointXYZI>());
                 pcl::transformPointCloud(*pointsBody, *pointsMap, Tb2m.cast<float>());
