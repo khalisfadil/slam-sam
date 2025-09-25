a04f09a21ed17b7c778adbf9a458bf531f1ec441
khalisfadil <muhammad.khalis.fadil@gmail.com>
2025-09-25 10:05:04 +0200

    update

--- run/pipeline.cpp
+++ run/pipeline.cpp
@@ -487,6 +487,8 @@ int main() {
                 const Eigen::Vector3d& lla = data_frame->position.back().pose;
                 const Eigen::Matrix3d& Cb2m = data_frame->position.back().orientation.toRotationMatrix().cast<double>();
                 Eigen::Matrix4d Tb2m = Eigen::Matrix4d::Identity();
+                const auto& insFactorStdDev = data_frame->position.back().poseStdDev;
+                insStdDev << insFactorStdDev.x(), insFactorStdDev.y(), insFactorStdDev.z(),0.01, 0.01, 0.01;
                 
 
                 uint64_t id = data_frame->points.frame_id;
@@ -564,8 +566,6 @@ int main() {
                     // Also add a GPS prior if the data is reliable.
                     if (data_frame->position.back().poseStdDev.norm() < 0.1f) {
                         gtsam::Pose3 insFactor(Tb2m);
-                        const auto& insFactorStdDev = data_frame->position.back().poseStdDev;
-                        insStdDev << insFactorStdDev.x(), insFactorStdDev.y(), insFactorStdDev.z(),0.01, 0.01, 0.01;
                         gtsam::SharedNoiseModel insNoiseModel = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(6) << 0.01, 0.01, 0.01, insFactorStdDev.x(), insFactorStdDev.y(), insFactorStdDev.z()).finished());
                         newFactors.add(gtsam::PriorFactor<gtsam::Pose3>(Symbol('x', id), std::move(insFactor), std::move(insNoiseModel)));
                     } 
