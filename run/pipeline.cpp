08dfdea889e75655ef66d377ae1c131e04ba5578
khalisfadil <muhammad.khalis.fadil@gmail.com>
2025-09-28 21:31:55 +0200

    update

--- run/pipeline.cpp
+++ run/pipeline.cpp
@@ -622,7 +622,7 @@ int main() {
                     //     newFactors.add(gtsam::BetweenFactor<gtsam::Pose3>(Symbol('x', last_id), Symbol('x', id), std::move(lidarFactor), std::move(lidarNoiseModel)));
                     // }
 
-                    bool is_gps_available_now = (insChecker < 0.09);
+                    bool is_gps_available_now = (insChecker < 0.1);
                     if (is_gps_available_now && was_gps_denied) {
                         std::cout << "Warning: GPS return from denied position.start trust gain recovery.\n";
                         current_trust_factor = 0.0; // Reset to begin recovery from zero trust
