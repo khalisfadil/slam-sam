3f75d98eb4cdd907ec364df6ea36ffc46a1574b7
khalisfadil <muhammad.khalis.fadil@gmail.com>
2025-09-29 22:44:52 +0200

    update

--- run/pipeline_calib_compass.cpp
+++ run/pipeline_calib_compass.cpp
@@ -75,7 +75,7 @@ int main() {
     //####################################################################################################
     auto calib_thread = std::thread([&compData]() {
         CompasHashMap compasArchive;
-        uint64_t id = 0;
+        uint64_t id = 47893;
         try {
             while (running) {
                 auto data_frame = compData.pop();
