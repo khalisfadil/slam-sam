657f87ce145b71bb6ee85aecd1fc5a025e8570ba
khalisfadil <muhammad.khalis.fadil@gmail.com>
2025-10-21 15:46:28 +0200

    update

--- run/pipeline_ins_map_distribution.cpp
+++ run/pipeline_ins_map_distribution.cpp
@@ -402,12 +402,31 @@ int main() {
     frameCompQueue.stop();
     frameDataQueue.stop();
 
-    if (lidar_iothread.joinable()) lidar_iothread.join();
+    std::cout << "Joining comp_iothread..." << std::endl;
     if (comp_iothread.joinable()) comp_iothread.join();
+    std::cout << "Joined comp_iothread." << std::endl;
+
+    std::cout << "Joining lidar_iothread..." << std::endl;
+    if (lidar_iothread.joinable()) lidar_iothread.join();
+    std::cout << "Joined lidar_iothread." << std::endl;
+
+    std::cout << "Joining lidar_processing_thread..." << std::endl;
     if (lidar_processing_thread.joinable()) lidar_processing_thread.join();
+    std::cout << "Joined lidar_processing_thread." << std::endl;
+
+    std::cout << "Joining comp_processing_thread..." << std::endl;
     if (comp_processing_thread.joinable()) comp_processing_thread.join();
+    std::cout << "Joining comp_processing_thread..." << std::endl;
+
+    std::cout << "Joining sync_thread..." << std::endl;
     if (sync_thread.joinable()) sync_thread.join();
+    std::cout << "Joining sync_thread..." << std::endl;
+
+    std::cout << "Joining cumm_thread..." << std::endl;
     if (cumm_thread.joinable()) cumm_thread.join();
+    std::cout << "Joining cumm_thread..." << std::endl;
+
+    std::cout << "All threads joined. Returning from main..." << std::endl;
     
     return 0;
 }
